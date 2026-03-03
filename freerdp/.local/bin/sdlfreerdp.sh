2c4ffe99b93ff70cd15565481b14020719fc78f4
skramsa <skramsa@gmail.com>
2026-03-03 10:22:27 +0100

    added rdp scripts

---
+++ freerdp/.local/bin/sdlfreerdp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+RDP_PASSWORD=$(zenity --password --title="RDP Password" --text="Enter password for RDP session:")
+
+if [ -n "$RDP_PASSWORD" ]; then
+    SDL_VIDEODRIVER=wayland sdl-freerdp /v:a-mgmt-atm.loc.ipfdd.de:3389 /u:a_atmanspacher /p:"$RDP_PASSWORD" +f +dynamic-resolution /kbd:layout:0x0407 +home-drive /d:LOC &
+fi
