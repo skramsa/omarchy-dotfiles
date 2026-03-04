604091c8e6b713da568eccb9e32859482b2c8172
skramsa <skramsa@gmail.com>
2026-03-04 10:52:46 +0100

    freerdp changes

--- freerdp/.local/bin/sdlfreerdp.sh
+++ freerdp/.local/bin/sdlfreerdp.sh
@@ -3,5 +3,5 @@
 RDP_PASSWORD=$(zenity --password --title="RDP Password" --text="Enter password for RDP session:")
 
 if [ -n "$RDP_PASSWORD" ]; then
-    SDL_VIDEODRIVER=wayland sdl-freerdp /v:a-mgmt-atm.loc.ipfdd.de:3389 /u:a_atmanspacher /p:"$RDP_PASSWORD" +f +dynamic-resolution /kbd:layout:0x0407 +home-drive /d:LOC &
+    SDL_VIDEODRIVER=wayland sdl-freerdp3 /v:a-mgmt-atm.loc.ipfdd.de:3389 /u:a_atmanspacher /p:"$RDP_PASSWORD" +dynamic-resolution /kbd:layout:0x0407 +home-drive /d:LOC &
 fi
